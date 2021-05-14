bf8f5e8d7a571877b07652a450b23bf0118c9825
Peter <dhondt.peter@gmail.com>
2021-05-14 22:14:00 +0200

    second challenge

--- Beverage.php
+++ Beverage.php
@@ -9,7 +9,7 @@ class Beverage
 
 
     /**
-     * beverage constructor.
+     * Beverage constructor.
      */
     public function __construct(string $color, float $price, string $temperature = 'cold')
     {
@@ -24,7 +24,39 @@ class Beverage
     }
 }
 
+class Beer extends Beverage
+{
+    public string $name;
+    public float $alcoholpercentage;
+
+    /**
+     * Beer constructor.
+     * @param string $name
+     * @param float $alcoholpercentage
+     */
+    public function __construct(string $color, float $price, string $temperature, string $name, float $alcoholpercentage)
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+        $this->name = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
+    }
+
+    public function getAlcoholpercentage(): float
+    {
+        return $this->alcoholpercentage;
+    }
+}
+
 $cola = new Beverage('black', 2);
 
 echo $cola->getInfo();
 echo $cola->temperature . PHP_EOL;
+
+$duvel =  new Beer('blond', 3.5, 'cold', 'Duvel', 8.5);
+echo $duvel->getAlcoholpercentage() . '%' . PHP_EOL;
+echo $duvel->alcoholpercentage . '%' . PHP_EOL;
+echo $duvel->color . PHP_EOL;
+echo $duvel->getInfo();
+echo $cola->getAlcoholpercentage();
